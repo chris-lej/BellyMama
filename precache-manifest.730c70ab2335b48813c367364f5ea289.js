d57b73a0285ec20e62f75b2b940283d67cdf559b
Christian <leje.chris@gmail.com>
2020-09-03 13:42:33 -0500

    Updates

---
+++ precache-manifest.730c70ab2335b48813c367364f5ea289.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eabb7c69e50974281f5b6e4e195f0bc7",
+    "url": "/BellyMama/index.html"
+  },
+  {
+    "revision": "56a66285bace40262845",
+    "url": "/BellyMama/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "60aa100c88806fcbd517",
+    "url": "/BellyMama/static/css/main.30804389.chunk.css"
+  },
+  {
+    "revision": "56a66285bace40262845",
+    "url": "/BellyMama/static/js/2.822a8d8b.chunk.js"
+  },
+  {
+    "revision": "60aa100c88806fcbd517",
+    "url": "/BellyMama/static/js/main.d83fbf55.chunk.js"
+  },
+  {
+    "revision": "c9d70e3e4a71a44c004e",
+    "url": "/BellyMama/static/js/runtime-main.f17d20aa.js"
+  },
+  {
+    "revision": "695c1b704f28b725ae3a6b31e697318c",
+    "url": "/BellyMama/static/media/facebook.695c1b70.svg"
+  },
+  {
+    "revision": "97d1974b85bc9e9db4d0f6c2209154eb",
+    "url": "/BellyMama/static/media/instagram.97d1974b.svg"
+  },
+  {
+    "revision": "4d17e56c8327e0ce812056c3399e6b7b",
+    "url": "/BellyMama/static/media/logo-simples.4d17e56c.svg"
+  },
+  {
+    "revision": "78b12a664a4f136b25fbc674313cdf91",
+    "url": "/BellyMama/static/media/placeholder.78b12a66.png"
+  }
+]);
